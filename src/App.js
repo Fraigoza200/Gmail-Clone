ad8bb6261ff6dc2dc321509e13057f0b7126fb14
Francisco Raigoza <raigoza.frankie@gmail.com>
2021-01-17 14:35:28 -0800

    Header section built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Gmail-clone</h2>
+    <div className="app">
+      <Header />
     </div>
   );
 }
