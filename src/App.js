ae5dc28f7f0cfd384f8a3245d534deb715560933
Francisco Raigoza <raigoza.frankie@gmail.com>
2021-01-17 16:14:24 -0800

    sidebar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Sidebar />
     </div>
   );
 }
