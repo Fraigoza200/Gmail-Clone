3fce552fb5f47494df2f8c2bb13071591ba3be67
Francisco Raigoza <raigoza.frankie@gmail.com>
2021-01-18 13:56:40 -0800

    creating mail component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header'
 import Sidebar from './Components/Sidebar'
+import Mail from './Components/Mail'
+import EmailList from './Components/EmailList'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
-  return (
+return (
+  <Router>
     <div className="app">
       <Header />
+
+      <div className="app__body">
       <Sidebar />
+
+      <Switch>
+        <Route path='/mail'>
+          <Mail />
+        </Route>
+        <Route path='/'>
+          <EmailList />
+        </Route>
+      </Switch>
+      </div>
     </div>
+  </Router>
   );
 }
 
